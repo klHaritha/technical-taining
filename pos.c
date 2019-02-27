846a3b27b247ea14e6e11243d972ec4347f314bd
klHaritha <48052673+klHaritha@users.noreply.github.com>
2019-02-28 03:22:44 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter the num:");
+    scanf("%d",&num);
+if(num>0)
+{
+    printf("positive");
+
+}
+else if(num==0){
+    printf("zero");
+    
+}
+else{
+    printf("negativa");
+}
+}
